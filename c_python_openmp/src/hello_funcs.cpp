a195f6d380ece3305a44dc529dccee87e269a2ab
Kaylan Burleigh <kburleigh@lbl.gov>
2018-07-11 15:48:15 -0700

    use git submodule for pybind11 to get CMakeLists.txt working

--- c_python_openmp/src/hello_funcs.cpp
+++ c_python_openmp/src/hello_funcs.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include "hello.h"
-//#include <pybind11/pybind11.h>
+#include <pybind11/pybind11.h>
 
 void myfunc() {
-  std::cout << "hello n from c++\n";
+  std::cout << "hello y from c++\n";
 }
 
-// namespace py = pybind11;
-//
-// PYBIND11_MODULE(hello_funcs, m) {
-//     m.doc() = "pybind11 example plugin"; // optional module docstring
-//
-//     m.def("myfunc", &myfunc, "A function which prints");
-// }
+namespace py = pybind11;
+
+PYBIND11_MODULE(hello_funcs, m) {
+    m.doc() = "pybind11 example plugin"; // optional module docstring
+
+    m.def("myfunc", &myfunc, "A function which prints");
+}
